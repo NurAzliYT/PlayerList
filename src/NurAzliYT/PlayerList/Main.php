8f7a60e5f5f13c9f9e14f6c03d6ddaca5c52a9ca
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-02 04:43:36 +0700

    Update Main.php

--- src/NurAzliYT/PlayerList/Main.php
+++ src/NurAzliYT/PlayerList/Main.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+by AEDXDV
+
+Youtube: @AEDXDEV
+Discord: aedxdev
+
+*/
 namespace NurAzliYT\PlayerList;
 
 use JaxkDev\DiscordBot\Models\Messages\Embed\Embed;
@@ -11,6 +19,7 @@ use JaxkDev\DiscordBot\Plugin\ApiResultion;
 use JaxkDev\DiscordBot\Plugin\ApiRejection;
 use JaxkDev\DiscordBot\Models\Messages\Message;
 
+use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
 use pocketmine\plugin\Plugin;
@@ -18,49 +27,50 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 
 class Main extends PluginBase implements Listener{
-
-    public DiscordBot $discord;
   
-    public function onEnable() : void{
-        $discord = $this->getServer()->getPluginManager()->getPlugin("DiscordBot");
-        if (!$discord instanceof DiscordBot){
-          $this->getLogger()->info("Incompatible dependency 'DiscordBot' detected, see https://github.com/DiscordBot-PMMP/DiscordBot/releases for the correct plugin.");
-          $this->getServer()->getPluginManager()->disablePlugin($this);
-          return;
-        }
-        $this->discord = $discord;
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  public DiscordBot $discord;
+  
+  public Config $config;
+  
+  public function onEnable() : void{
+    $discord = $this->getServer()->getPluginManager()->getPlugin("DiscordBot");
+    if (!$discord instanceof DiscordBot){
+      $this->getLogger()->info("Incompatible dependency 'DiscordBot' detected, see https://github.com/DiscordBot-PMMP/DiscordBot/releases for the correct plugin.");
+      $this->getServer()->getPluginManager()->disablePlugin($this);
+      return;
     }
-    public function MessageSent(MessageSent $event){
-        $api = $this->discord->getApi();
-        $message = $event->getMessage();
-        $content = $message->getContent();
-        $channel_id = $message->getChannelId();
-        $args = explode(" ", $content);
-        // command
-        if(isset($args[0]) && $args[0] == "!list"){
-          $onlines = $this->getServer()->getOnlinePlayers();
-          $p = count($onlines) . "/" . $this->getServer()->getMaxPlayers();
-          $players =  implode("\n", array_map(fn(Player $player) => $player->getName(), $onlines));
-          $api->sendMessage(null, $channel_id, null, $message->getAuthorId(), [new Embed(
-            "List Players",
-            count($onlines) === 0 ? "There Are No Players In The Server" : $p,
-            null, time(), null,
-            new Footer("List Players v" . $this->getDescription()->getVersion()),
-            null, null, null, null, null,
-            count($onlines) === 0 ? [] : [new Field("Players", $players, true)]
-          )])->otherwise(function(ApiRejection $rejection){
-            $this->getLogger()->error("Failed to send command response: " . $rejection->getMessage());
-          });
-          /*$api->fetchChannel(null, $channel_id)->then(function(ApiResultion $resultion) use($api, $channel_id, $message) {
-            $channel = $resultion->getData()[0];
-            $server_id = $channel->getGuildId();
-            
-            $api->sendMessage($server_id, $channel_id, $p . $m, $message->getAuthorId());
-          }, function(ApiRejection $rejection) /*use($api, $channel_id, $message){
-            $this->getLogger()->error("Failed to send command response: " . $rejection->getMessage());
-            $this->getServer()->getPluginManager()->disablePlugin($this);
-          });*/
-        }
+    $this->discord = $discord;
+    $this->config = new Config($this->getDataFolder() . "config.yml", 2, ["ServerId" => 1234567890]);
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function MessageSent(MessageSent $event){
+    $api = $this->discord->getApi();
+    $message = $event->getMessage();
+    $content = $message->getContent();
+    $channel_id = $message->getChannelId();
+    $args = explode(" ", $content);
+    // command
+    if(isset($args[0]) && $args[0] == "!list"){
+      $onlines = $this->getServer()->getOnlinePlayers();
+      $p = count($onlines) . "/" . $this->getServer()->getMaxPlayers();
+      $players =  implode("\n", array_map(fn(Player $player) => $player->getName(), $onlines));
+      $server_id = $this->config->get("ServerId", 1234567890);
+      if ($server_id == 1234567890){
+        $this->getLogger()->info("§cPlease Add Discord Server Id In config.yml");
+        $this->getServer()->getPluginManager()->disablePlugin($this);
+        return false;
+      }
+      $api->sendMessage($server_id, $channel_id, null, $message->getAuthorId(), [new Embed(
+        "List Players",
+        count($onlines) === 0 ? "There Are No Players In The Server" : $p,
+        null, time(), null,
+        new Footer("List Players v" . $this->getDescription()->getVersion()),
+        null, null, null, null, null,
+        count($onlines) === 0 ? [] : [new Field("Players", $players, true)]
+      )])->otherwise(function(ApiRejection $rejection){
+        $this->getLogger()->error("Failed to send command response: " . $rejection->getMessage());
+      });
     }
+  }
 }
